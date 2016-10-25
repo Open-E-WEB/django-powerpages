fc4e700c34780706b9e814cb08e1d32422d80bd4
Pawel Scierski <pawel.scierski@open-e.com>
2016-10-25 15:23:05 +0200

    v0.0.4

--- setup.py
+++ setup.py
@@ -101,7 +101,7 @@ class MakeMigrations(DjangoCommand):
 
 setup(
     name='django-powerpages',
-    version='0.0.3',
+    version='0.0.4',
     description=(
         'Developer-friendly, simple CMS for Django, "flatpages on steroids".'
     ),
